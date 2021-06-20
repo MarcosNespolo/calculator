d5aba8c80d45373e9bb70740f23c84e7fae72502
Gabriel Sestrem <gabrielmmsestrem@gmail.com>
2021-06-20 15:00:11 +1200

    Update api.ts
    New url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://backend-gabrielsestrem.vercel.app/api/pricing/',
+  baseURL: 'https://www.linkyouweb.com.br/api/',
   headers: {
     'Content-Type': 'application/json',
   },
